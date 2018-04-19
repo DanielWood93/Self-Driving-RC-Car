7aff83719ca2ca087909ef6b62fa7cfd223f781f
Daniel Wood <daniel.93.wood@gmail.com>
2018-04-19 01:11:21 +0000

    Add logging and threading to frame.py and ultrasonic.py, add drive.py

--- threaded_app/frame.py
+++ threaded_app/frame.py
@@ -11,12 +11,12 @@ Example:
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from threading import Thread
-import numpy as np
 import cv2
 import time
+import logging
 
 
-class Frame:
+class Frame(Thread):  # try Frame(Thread)
     def __init__(self, thread_id, name, resolution=(320, 240), framerate=30):
         """Initialize pi camera settings and begin stream
         Args:
@@ -25,6 +25,8 @@ class Frame:
             resolution: resolution of camera image, defaults to (320, 240)
             framerate: framerate of stream, defaults to 30
         """
+        logging.info('__init__')
+        super(Frame, self).__init__()
         self.thread_id = thread_id
         self.name = name
         self.camera = PiCamera()
@@ -36,11 +38,13 @@ class Frame:
         self.stopped = False
 
     def start(self):
+        logging.info('start')
         Thread(target=self.update, args=()).start()
         return self
 
     def update(self):
         """Update stream with next frame"""
+        logging.info('starting self')
         for f in self.stream:
             self.frame = f.array
             self.rawCapture.truncate(0)
@@ -52,6 +56,7 @@ class Frame:
 
     def read(self):
         """Read and return latest frame from stream"""
+        #logging.info('read')
         return self.frame
 
     def process(self, original):
@@ -61,6 +66,7 @@ class Frame:
         Returns:
             canny_edges: returns masked region of interest image with canny edge detection and center lines drawn
         """
+        #logging.info('process')
         greyscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)  # convert to greyscale
         masked_white = cv2.inRange(greyscale, 200, 255)     # mask white pixels of greyscale image
         blurred = cv2.GaussianBlur(masked_white, (5, 5), 0)    # apply gaussian blur
@@ -74,30 +80,44 @@ class Frame:
             filename: file name to save as
             image: image to save
         """
+        logging.info('save_image')
         cv2.imwrite(filename, image)
 
     def stop_stream(self):
         """Stop stream"""
+        logging.info('stop_stream')
         self.stopped = True
 
 
-def main():
-    stream = Frame(1, "ProcessFrame")
-    stream.start()
-    time.sleep(2.0)
-    while 1:
-        frame = stream.read()
-        halved = frame[120:240, 0:320]
-        edges = stream.process(halved)
-        
-        cv2.imshow("Canny Edges", edges)
-        cv2.waitKey(1) & 0xFF
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+)
 
-    cv2.destroyAllWindows()
-    stream.stop_stream()
-    print("-----END-----")
-    exit()
+
+def main():
+    try:
+        stream = Frame(1, "ProcessFrame")
+        stream.start()
+        time.sleep(1)
+        logging.info('start loop')
+        while 1:
+            frame = stream.read()
+            halved = frame[120:240, 0:320]
+            processed = stream.process(halved)
+
+            cv2.imshow('Processed Frame', processed)
+            cv2.waitKey(1) & 0xFF
+
+    except KeyboardInterrupt:
+        logging.info('KeyboardInterrupt')
+        cv2.destroyAllWindows()
+        stream.stop_stream()
+        exit()
 
 
 if __name__ == '__main__':
     main()
+
+
+# make a pause thing
--- threaded_app/ultrasonic.py
+++ threaded_app/ultrasonic.py
@@ -1,16 +1,28 @@
 import RPi.GPIO as GPIO
 from time import sleep, time
+from threading import Thread
+import logging
 
 
-class Ultrasonic:
-    def __init__(self, trig_pin, echo_pin):
+class Ultrasonic(Thread):
+    def __init__(self, thread_id, name, trig_pin, echo_pin):
+        logging.info('__init__')
+        super(Ultrasonic, self).__init__()
+        self.thread_id = thread_id
+        self.name = name
         self.trig = trig_pin
         self.echo = echo_pin
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.trig, GPIO.OUT)
         GPIO.setup(self.echo, GPIO.IN)
 
+    def start(self):
+        logging.info('start')
+        Thread(target=self.measure(), args=()).start()
+        return self
+
     def measure(self):
+        # logging.info('measure')
         GPIO.output(self.trig, GPIO.LOW)
         sleep(0.1)
         GPIO.output(self.trig, GPIO.HIGH)
@@ -19,23 +31,34 @@ class Ultrasonic:
 
         while GPIO.input(self.echo) == GPIO.LOW:
             pulse_start = time()
-
         while GPIO.input(self.echo) == GPIO.HIGH:
             pulse_end = time()
-
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 17160.5
         distance = round(distance, 2)
         return distance
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+)
+
+
 def main():
     try:
-        sensor = Ultrasonic(23, 24)
+        sensor = Ultrasonic(1, "Ultrasonic", 23, 24)
+        sensor.start()
         for x in range(0, 5):
             print(sensor.measure(), "cm")
+
+    except KeyboardInterrupt:
+        logging.info('KeyboardInterrupt')
+
     finally:
         GPIO.cleanup()  # cleanup GPIO ports after use
+        logging.info('Exiting')
+        exit()
 
 
 if __name__ == '__main__':
